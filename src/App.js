6f7bb57e68b6b49d1c7f7a1596da030795fa665b
Sharad Rao <sharad011989@gmail.com>
2024-06-27 10:03:20 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-4xl font-bold text-center mb-4">Task Manager</h1>
+      <TaskForm />
+      <TaskList />
     </div>
   );
 }
